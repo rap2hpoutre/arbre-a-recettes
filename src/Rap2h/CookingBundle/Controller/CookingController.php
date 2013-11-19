5185a0dbb39478c9ba8ad76b661c575f0070927f
rap2hpoutre <raphaelht@gmail.com>
2013-11-18 22:56:34 -0800

    Initial commit 3

--- src/Rap2h/CookingBundle/Controller/CookingController.php
+++ src/Rap2h/CookingBundle/Controller/CookingController.php
@@ -9,17 +9,25 @@ use Symfony\Component\HttpFoundation\Session\Session;
 class CookingController extends Controller {
 
 	public function indexAction() {
+		return $this->render('Rap2hCookingBundle:Cooking:index.html.twig');
+	}
+
+	public function aboutAction() {
+		return $this->render('Rap2hCookingBundle:Cooking:about.html.twig');
+	}
+
+	public function startAction() {
 		$session = $this->get('session');
 		$session->start();
 		$items = $session->get('recipeItems');
-		return $this->render('Rap2hCookingBundle:Cooking:index.html.twig', array("recipeItems" => $items));
+		return $this->render('Rap2hCookingBundle:Cooking:start.html.twig', array("recipeItems" => $items));
 	}
 
 	public function resetRecipeAction() {
 		$session = $this->get('session');
 		$session->start();
 		$session->set('recipeItems', null);
-		return $this->redirect($this->generateUrl('CookingIndex'));
+		return $this->redirect($this->generateUrl('CookingStart'));
 	}
 
 	public function addRecipeItemAction($recipeItemId) {
@@ -32,7 +40,7 @@ class CookingController extends Controller {
 
 		$session->getFlashBag()->add('info', 'Tu viens d\'ajouter une ' . $recipeItemId);
 
-		return $this->redirect($this->generateUrl('CookingIndex'));
+		return $this->redirect($this->generateUrl('CookingStart'));
 
 	}
 }
